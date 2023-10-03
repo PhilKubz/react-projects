7d478b908a7fe5a9e612c17c47f39a90251f441c
Phil <philip.kubisz@gmail.com>
2023-10-02 19:22:23 -0700

    starting project structure from scratch

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,24 +1,9 @@
-import './App.css';
+import React from 'react';
 
-// Component
-const Person = (props) => {
-  return (
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name: {props.lastName}</h2>
-    <h2>Age: {props.age}</h2>
-    </>
-  )
-}
-
-// What Webpage renders (where components are injected)
 const App = () => {
-return (
-    <div className="App">
-      <Person name={'Phil'} lastName={'Kubisz'} age={'32'}/>
-      <Person />
-    </div>
-  );
+    return (
+        <h1>App</h1>
+    );
 }
 
 export default App;
